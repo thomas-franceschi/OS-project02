87425954503a5d826a276c44aa26e720c8b05e90
Kyle Williams <kwilli20@nd.edu>
2016-09-28 17:26:47 -0400

    list constructor/destructor

--- list.cpp
+++ list.cpp
@@ -5,9 +5,20 @@
 #include <memory>
 
 List::List() {
+
+	head = nullptr;
+	length = 0;
 }
 
 List::~List() {
+
+	Node *next = nullptr;
+
+	for( Node *curr = head; curr != nullptr; curr = next ) {
+		next = curr->next;
+		delete curr;
+	}
+	
 }
 
 void List::push_front(const std::string &s) {
